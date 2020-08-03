41be62a2f157cc1323e824d6dbb885bff25fd54e
Scala Steward <me@scala-steward.org>
2020-08-04 00:21:05 +0200

    Update kafka, kafka-streams to 2.6.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   object Versions {
     val Scala                = "2.13.2"
     val Scala212             = "2.12.11"
-    val Kafka                = "2.5.0"
+    val Kafka                = "2.6.0"
     val Slf4j                = "1.7.30"
     val ScalaTest            = "3.2.0"
     val ScalaTestPlusMockito = "3.2.0.0"
