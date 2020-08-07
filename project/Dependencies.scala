fd6330b36b77e45d5d0c5bd435b42dba96fddd6e
Scala Steward <me@scala-steward.org>
2020-08-07 06:43:29 +0200

    Update mockito-3-3 to 3.2.1.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
     val Kafka                = "2.5.0"
     val Slf4j                = "1.7.30"
     val ScalaTest            = "3.2.0"
-    val ScalaTestPlusMockito = "3.2.0.0"
+    val ScalaTestPlusMockito = "3.2.1.0"
   }
 
   object Common {
