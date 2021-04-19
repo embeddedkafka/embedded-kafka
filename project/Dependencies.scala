cdf7608fc0e96769670db1e6943ec7fdd468ed5c
Scala Steward <me@scala-steward.org>
2021-04-19 08:20:49 +0200

    Update connect-runtime, kafka, ... to 2.8.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   object Versions {
     val Scala                = "2.13.4"
     val Scala212             = "2.12.12"
-    val Kafka                = "2.7.0"
+    val Kafka                = "2.8.0"
     val Slf4j                = "1.7.30"
     val ScalaTest            = "3.2.7"
     val ScalaTestPlusMockito = "3.2.7.0"
