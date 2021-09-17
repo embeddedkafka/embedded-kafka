f41b2543c2f8cd769d5d8b5efb6fb23adacef622
Scala Steward <me@scala-steward.org>
2021-09-17 23:22:16 +0200

    Update connect-runtime, kafka, ... to 2.8.1

--- embedded-kafka/src/main/scala/net/manub/embeddedkafka/package.scala
+++ embedded-kafka/src/main/scala/net/manub/embeddedkafka/package.scala
@@ -2,7 +2,7 @@ package net.manub
 
 package object embeddedkafka {
 
-  @deprecated("Use io.github.embeddedkafka.EmbeddedKafka instead", "2.8.0")
+  @deprecated("Use io.github.embeddedkafka.EmbeddedKafka instead", "2.8.1")
   type EmbeddedKafka = io.github.embeddedkafka.EmbeddedKafka
   @deprecated(
     "Use io.github.embeddedkafka.EmbeddedKafkaConfig instead",
@@ -14,9 +14,9 @@ package object embeddedkafka {
     "2.8.0"
   )
   type EmbeddedKafkaConfigImpl = io.github.embeddedkafka.EmbeddedKafkaConfigImpl
-  @deprecated("Use io.github.embeddedkafka.EmbeddedZ instead", "2.8.0")
+  @deprecated("Use io.github.embeddedkafka.EmbeddedZ instead", "2.8.1")
   type EmbeddedZ = io.github.embeddedkafka.EmbeddedZ
-  @deprecated("Use io.github.embeddedkafka.EmbeddedK instead", "2.8.0")
+  @deprecated("Use io.github.embeddedkafka.EmbeddedK instead", "2.8.1")
   type EmbeddedK = io.github.embeddedkafka.EmbeddedK
   @deprecated(
     "Use io.github.embeddedkafka.KafkaUnavailableException instead",
@@ -25,22 +25,22 @@ package object embeddedkafka {
   type KafkaUnavailableException =
     io.github.embeddedkafka.KafkaUnavailableException
 
-  @deprecated("Use io.github.embeddedkafka.Codecs instead", "2.8.0")
+  @deprecated("Use io.github.embeddedkafka.Codecs instead", "2.8.1")
   val Codecs = io.github.embeddedkafka.Codecs
-  @deprecated("Use io.github.embeddedkafka.ConsumerExtensions instead", "2.8.0")
+  @deprecated("Use io.github.embeddedkafka.ConsumerExtensions instead", "2.8.1")
   val ConsumerExtensions = io.github.embeddedkafka.ConsumerExtensions
-  @deprecated("Use io.github.embeddedkafka.EmbeddedKafka instead", "2.8.0")
+  @deprecated("Use io.github.embeddedkafka.EmbeddedKafka instead", "2.8.1")
   val EmbeddedKafka = io.github.embeddedkafka.EmbeddedKafka
   @deprecated(
     "Use io.github.embeddedkafka.EmbeddedKafkaConfig instead",
     "2.8.0"
   )
   val EmbeddedKafkaConfig = io.github.embeddedkafka.EmbeddedKafkaConfig
-  @deprecated("Use io.github.embeddedkafka.EmbeddedZ instead", "2.8.0")
+  @deprecated("Use io.github.embeddedkafka.EmbeddedZ instead", "2.8.1")
   val EmbeddedZ = io.github.embeddedkafka.EmbeddedZ
-  @deprecated("Use io.github.embeddedkafka.EmbeddedK instead", "2.8.0")
+  @deprecated("Use io.github.embeddedkafka.EmbeddedK instead", "2.8.1")
   val EmbeddedK = io.github.embeddedkafka.EmbeddedK
-  @deprecated("Use io.github.embeddedkafka.UUIDs instead", "2.8.0")
+  @deprecated("Use io.github.embeddedkafka.UUIDs instead", "2.8.1")
   val UUIDs = io.github.embeddedkafka.UUIDs
 
 }
