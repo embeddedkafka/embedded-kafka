042da0e99541937dc12c2ca65e8eff8aa62548b6
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-01-26 14:58:47 +0100

    Update connect-runtime, kafka, ... to 3.1.0 (#347)
    * Update connect-runtime, kafka, ... to 3.1.0
    
    * Update connect-runtime, kafka, ... to 3.1.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   object Versions {
     val Scala     = "2.13.8"
     val Scala212  = "2.12.15"
-    val Kafka     = "3.0.0"
+    val Kafka     = "3.1.0"
     val Slf4j     = "1.7.35"
     val ScalaTest = "3.2.11"
   }
