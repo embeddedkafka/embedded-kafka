b25c56f9dfc67e1ab2a9f11b1d07610cff1f4381
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-08-02 09:18:58 +0200

    Update connect-runtime, kafka, ... to 3.2.1 (#384)
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   object Versions {
     val Scala     = "2.13.8"
     val Scala212  = "2.12.16"
-    val Kafka     = "3.2.0"
+    val Kafka     = "3.2.1"
     val Slf4j     = "1.7.36"
     val ScalaTest = "3.2.13"
   }
