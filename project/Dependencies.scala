fcd19fb8e658372a5032c8ad07dd6484df76eb96
Francesco Pellegrini <francesco.pelle@gmail.com>
2022-10-09 09:50:16 +0200

    bump kafka to v3.2.3 (#393)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   object Versions {
     val Scala     = "2.13.8"
     val Scala212  = "2.12.16"
-    val Kafka     = "3.2.2"
+    val Kafka     = "3.2.3"
     val Slf4j     = "1.7.36"
     val ScalaTest = "3.2.13"
   }
