58ff9817b65930c6038e8e452dd8084d5499c1c3
Scala Steward <scala_steward@virtuslab.com>
2022-10-19 17:29:33 +0000

    Update scala-library to 2.12.17

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
 
   object Versions {
     val Scala     = "2.13.8"
-    val Scala212  = "2.12.16"
+    val Scala212  = "2.12.17"
     val Kafka     = "3.3.1"
     val Slf4j     = "1.7.36"
     val ScalaTest = "3.2.14"
