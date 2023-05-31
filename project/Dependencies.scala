ee9c347c8bdd802b6c5261613eb69d99916d8674
Scala Steward <scala_steward@virtuslab.com>
2023-05-31 15:43:31 +0000

    Update scala3-library to 3.3.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
 
   object Versions {
-    val Scala3    = "3.2.2"
+    val Scala3    = "3.3.0"
     val Scala213  = "2.13.10"
     val Scala212  = "2.12.17"
     val Kafka     = "3.4.0"
