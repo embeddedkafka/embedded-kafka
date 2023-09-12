a4d3a7f0642a86c9095aa78ddd2e4736484dbba9
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2023-09-12 21:04:58 +0200

    Update scala-library to 2.13.12 (#473)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
 
   object Versions {
     val Scala3    = "3.3.1"
-    val Scala213  = "2.13.11"
+    val Scala213  = "2.13.12"
     val Scala212  = "2.12.18"
     val Kafka     = "3.5.1"
     val Slf4j     = "1.7.36"
