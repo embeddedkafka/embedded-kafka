2b0105a27e85a36f4976130f1afcf70f374bb149
Scala Steward <scala_steward@virtuslab.com>
2023-10-04 14:04:49 +0000

    Update connect-runtime, kafka, ... to 3.6.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
     val Scala3    = "3.3.1"
     val Scala213  = "2.13.12"
     val Scala212  = "2.12.18"
-    val Kafka     = "3.5.1"
+    val Kafka     = "3.6.0"
     val Slf4j     = "1.7.36"
     val ScalaTest = "3.2.17"
   }
