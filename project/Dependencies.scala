28b8a46e8d9e789f867115b659ccfd0046b8e2df
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2025-05-09 07:37:47 +0200

    Update scala3-library to 3.3.6 (#568)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
 
   object Versions {
-    val Scala3    = "3.3.5"
+    val Scala3    = "3.3.6"
     val Scala213  = "2.13.16"
     val Kafka     = "4.0.0"
     val Slf4j     = "1.7.36"
