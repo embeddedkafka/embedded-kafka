1fd322d82a127ce86f5492547a29b12dd736d742
Gaël Jourdan-Weil <gjourdanweil@gmail.com>
2025-09-05 18:42:00 +0200

    🧹 chore: upgrade SLF4J dependency (#603)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
     val Scala3    = "3.3.6"
     val Scala213  = "2.13.16"
     val Kafka     = "4.1.0"
-    val Slf4j     = "1.7.36"
+    val Slf4j     = "2.0.17"
     val ScalaTest = "3.2.19"
     val Jackson   = "2.19.0" // Keep consistent with the one provided by Kafka
   }
