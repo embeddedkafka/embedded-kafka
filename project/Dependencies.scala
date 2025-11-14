3e532bb443d1ddb8924f233bb67a4725851fde5e
Scala Steward <scala_steward@virtuslab.com>
2025-11-14 15:14:22 +0000

    Update connect-runtime, kafka, ... to 4.1.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   object Versions {
     val Scala3    = "3.3.7"
     val Scala213  = "2.13.17"
-    val Kafka     = "4.1.0"
+    val Kafka     = "4.1.1"
     val Slf4j     = "2.0.17"
     val ScalaTest = "3.2.19"
     val Jackson   = "2.19.0" // Keep consistent with the one provided by Kafka
