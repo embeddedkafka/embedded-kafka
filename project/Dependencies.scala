3d7affb965e124a555b45d9938a98924301265b7
Scala Steward <scala_steward@virtuslab.com>
2026-09-22 15:46:00 +0000

    Update jackson-module-scala to 2.21.7

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
     val Kafka     = "4.3.1"
     val Slf4j     = "2.0.19"
     val ScalaTest = "3.2.20"
-    val Jackson   = "2.21.2" // Keep consistent with the one provided by Kafka
+    val Jackson   = "2.21.7" // Keep consistent with the one provided by Kafka
   }
 
   object Common {
